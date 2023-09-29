522fa74b34cc3e3ab23ef2510863bbffff9eb770
Veljko Radovic <veljkorad24@gmail.com>
2023-09-29 16:07:14 +0200

    Online Prodavnica
    Created a website with friends for subject exam, where i wrote the whole code while the design and animation were created by Jovana Maletin and Vuk Babic

---
+++ Online_Prodavnica/js/shopItems.js
@@ -0,0 +1,115 @@
+var shopItems = [
+				{
+					"itemImg": "shopimgs/nezuko.png",
+					"itemDesc": "<h3>Nezuko Figura</h3>",
+					"itemPrice": 179,		
+					"itemId":"InfFig_Nezuko",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/astoflo.png",
+					"itemDesc": "<h3>Astolfo Figura</h3>",
+					"itemPrice": 149,
+					"itemId":"InfFig_Astolfo",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/kujo.png",
+					"itemDesc": "<h3>Kujo Sticker</h3>",
+					"itemPrice": 5,
+					"itemId":"InfFig_Kujo",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/DioPoza.png",
+					"itemDesc": "<h3>Dio stiker</h3>",
+					"itemPrice": 8,
+					"itemId":"InfFig_Poza",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/opanci.png",
+					"itemDesc": "<h3>Dio Opanci</h3>",
+					"itemPrice": 690,
+					"itemId":"InfFig_Opanci",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/Katana.png",
+					"itemDesc": "<h3>Katana</h3>",
+					"itemPrice": 369,
+					"itemId":"InfFig_katana",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/Ambu.png",
+					"itemDesc": "<h3>Maska(Crna)</h3><p></p>",
+					"itemPrice": 29,
+					"itemId":"InfFig_ambu",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/Ambu2.png",
+					"itemDesc": "<h3>Maska(Bela)</h3>",
+					"itemPrice": 29,
+					"itemId":"InfFig_ambu2",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/HxH.png",
+					"itemDesc": "<h3>Hunter x Hunter</h3>",
+					"itemPrice": 5,
+					"itemId":"InfFig_kartice",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/JujutsuKaisen.png",
+					"itemDesc": "<h3>Jujutsu privesci</h3>",
+					"itemPrice": 9,
+					"itemId":"InfFig_jujutsu",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/Levi.png",
+					"itemDesc": "<h3>AOT majica</h3>",
+					"itemPrice": 17,
+					"itemId":"InfFig_aot",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/Mayuri.png",
+					"itemDesc": "<h3>Steins;gate</h3>",
+					"itemPrice": 18,
+					"itemId":"InfFig_gate",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/SailorMoon.png",
+					"itemDesc": "<h3>SailorMoon</h3>",
+					"itemPrice": 19,
+					"itemId":"InfFig_moon",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/ZmajevaKugla.png",
+					"itemDesc": "<h3>DB privezak</h3>",
+					"itemPrice": 8,
+					"itemId":"InfFig_ball",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/Titan.png",
+					"itemDesc": "<h3>AOT solja</h3>",
+					"itemPrice": 199,
+					"itemId":"InfFig_solja",
+					"itemCount":1
+				},
+				{
+					"itemImg": "shopimgs/Uzui.png",
+					"itemDesc": "<h3>Uzui stiker</h3>",
+					"itemPrice": 9,
+					"itemId":"InfFig_uzui",
+					"itemCount":1
+				},
+				
+			];
